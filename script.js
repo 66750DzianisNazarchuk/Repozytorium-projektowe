adb22a1880fccae37398b512edb76a38c634db2d
66750DzianisNazarchuk <dnazarc1@stu.vistula.edu.pl>
2026-04-19 22:31:46 +0200

    Create script.js 66750

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+  
+    const btnTheme = document.getElementById("btn-theme");
+    let isDefaultRed = true; 
+
+    btnTheme.addEventListener("click", function() {
+        if (isDefaultRed) {
+            document.body.style.backgroundColor = "darkgreen";
+            document.body.style.color = "white"; 
+            isDefaultRed = false;
+        } else {
+            document.body.style.backgroundColor = "darkred";
+            document.body.style.color = "white";
+            isDefaultRed = true;
+        }
+    });
+
+    const btnToggle = document.getElementById("btn-toggle");
+    const sekcjaProjekty = document.getElementById("sekcja-projekty");
+
+    btnToggle.addEventListener("click", function() {
+        if (sekcjaProjekty.style.display === "none") {
+            sekcjaProjekty.style.display = "block"; 
+        } else {
+            sekcjaProjekty.style.display = "none";  
+        }
+    });
+});sc
